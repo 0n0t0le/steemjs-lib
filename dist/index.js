7e35c1a3bc346f602b45ccca4446100640d355c8
fmiboy <fmiboy@mail.ru>
2016-08-10 17:15:32 +0300

    test build

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var objectAssign = require('object-assign');
+module.exports = objectAssign(require("./chain"), require("./ecc"), require("./serializer"));
+/*var chain = require("./chain");
+var ecc = require("./ecc");
+var serializer = require("./serializer");
+module.exports = function (app) {
+	app.chain = chain;
+	app.ecc = ecc;
+	app.serializer = serializer;
+};*/
